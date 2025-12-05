072fef2ec9c2ea314d8c8d40afae18b24bf18699
Zach Shuffield <zshuff03@yahoo.com>
2025-12-05 11:35:38 -0500

    finished

--- 2025/day5/index.js
+++ 2025/day5/index.js
@@ -1,29 +1,42 @@
 const { stringInput } = require('./input.js');
 const fs = require('fs');
 const DBfile = stringInput.split('\n');
-
-const freshIngredients = [];
-const ranges = [];
+let total = 0;
+let ranges = [];
 for (let x = 0; x < DBfile.length; x++) {
-    console.log('processing line', DBfile[x]);
     const dbLine = DBfile[x];
     if (dbLine.includes('-')) {
-        console.log('range found', dbLine);
-        ranges.push(dbLine.split('-').map(Number));
+        
+        const newRange = dbLine.split('-').map(Number)
+        ranges.push(newRange);
         continue;
     }
-    const ingredientId = parseInt(dbLine, 10);
-    if (!isNaN(ingredientId)) {
-        console.log('checking ingredient id', ingredientId);
-        for(let i = 0; i < ranges.length; i++) {
-            if (ingredientId >= ranges[i][0] && ingredientId <= ranges[i][1]) {
-                console.log('ingredient id is in range', ranges[i], `pushing ${ingredientId} to freshIngredients`);
-                freshIngredients.push(ingredientId);
-                break;
-            }
-        }
+    break;
+}
+ranges = ranges.sort((a, b) => a[0] - b[0]);
+const dedupedRanges = [ranges[0]];
+
+for (let j = 1; j < ranges.length; j++) {
+    const testingRange = dedupedRanges[dedupedRanges.length - 1];
+
+    if (testingRange[1] >= ranges[j][0]) {
+        testingRange[1] = Math.max(testingRange[1], ranges[j][1]);
+    } else {
+        dedupedRanges.push(ranges[j]);
     }
 }
-console.log(`${freshIngredients.length}`);
 
-fs.writeFileSync("./log.json", JSON.stringify({ranges, freshIngredients}, null, 3));
+for (const range of dedupedRanges) {
+    const totalNumInRange = countNumbersInclusive(range[0], range[1]);
+    total += totalNumInRange;
+}
+function countNumbersInclusive(num1, num2) {
+  const min = Math.min(num1, num2) - 1;
+  const max = Math.max(num1, num2) + 1;
+
+  // If max - min is less than 2, there are no numbers exclusively between them.
+  return Math.max(0, max - min - 1); 
+}
+console.log(`${total}`);
+
+fs.writeFileSync("./log.json", JSON.stringify({ranges, dedupedRanges}, null, 3));
